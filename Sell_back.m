8a2f6cd2a176f2b0e3cc944058ea540ee429888e
ZhengyiWang <wbswzy@163.com>
2017-09-04 23:37:47 +0800

    Update Sell_back
    更改了函数的返回值，使其不再是对后180天整体的判断，而是返回一个具体的满足回售条款的日期

--- Sell_back.m
+++ Sell_back.m
@@ -1,40 +1,37 @@
-function flag = Sell_back( Dayrange,Duration,Exchangeprice,Price )
-%�ڷ����˱��ڷ��еĿɽ���ծȯ����ǰ180���ڣ�
-%�����Ĺ�Ʊ���κ�����15�������յ����̼۸���ڵ��ڻ��ɼ۵�80%ʱ��
-%ծȯ��������Ȩ������еĿɽ���ծȯȫ���򲿷ְ���ֵ���ϵ���Ӧ����Ϣ�ļ۸���۸���˾�� 
+function [Sellday] = Sell_back( Dayrange,Duration,Exchangeprice,Price )
+%在发行人本期发行的可交换债券到期前180天内，
+%如果标的股票在任何连续15个交易日的收盘价格低于当期换股价的80%时，
+%债券持有人有权将其持有的可交换债券全部或部分按面值加上当期应计利息的价格回售给公司。 
 
-%�ο�Exchaneprice������д����������15��ת������15������15������������
+%参考Exchaneprice函数的写法，将连续15日转变成如果15日内有15日满足条件。
 
-%�������壺 ExchangepriceΪ���ɼ۸�DayrangeΪ�ж������䣬��Ӧ��180��������
-%DurationΪ��Ҫ�������������Ӧ15��������,priceΪ��Ʊ�۸����顣80%ֱ��д�뺯����
+%参数含义： Exchangeprice为换股价格，Dayrange为判定的区间，对应后180个交易日
+%Duration为需要满足的条件，对应15个交易日,price为股票价格数组。80%直接写入函数中
 
-%��������������򷵻�2������������������򷵻�0
+%如果满足条件，则返回日期；如果不满足条件，则返回EB摘牌日
 
 Length=length(Price);
-count=0; %���ڼ�¼��������������
-flag=0; %���ڱ���Ƿ���������
+count=0; %用于记录满足条件的天数
 
-for i=Length-Dayrange:Duration
-    if (Price(i)<Exchangeprice*0.8)
+for Sellday=Length-Dayrange:Duration+Length-Dayrange
+    if (Price(Sellday)<Exchangeprice*0.8)
         count=count+1;
     end
 end
 
 if count==Duration
-    flag=2;
     return;
 end
 
-for i=Duration+1:Length
-    if (Price(i)<Exchangeprice*0.8)
+for Sellday=Duration+1+Length-Dayrange:Length
+    if (Price(Sellday)<Exchangeprice*0.8)
         count=count+1;
     end
-    if(Price(i-Duration)<Exchangeprice*0.8)
+    if(Price(Sellday-Duration)<Exchangeprice*0.8)
         count=count-1;
     end
-    if count==Duration
-        flag=2;
-        break;
+    if count>=Duration
+        return;
     end
 end
 end
